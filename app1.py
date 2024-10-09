51f20767df4c504b7931aa67c2bd89d3add4f493
Satyajeet-5 <ghoshsatyajeet7@gmail.com>
2024-10-09 21:45:37 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -12,12 +12,12 @@ def index():
 @app.route('/compare_faces', methods=['POST'])
 def compare_faces():
     # Retrieve the uploaded images
-    image1 = request.files['image1']
-    image2 = request.files['image2']
+    img1 = request.files['image1']
+    img2 = request.files['image2']
     
     # Convert uploaded images to OpenCV format
-    img1 = cv2.imdecode(np.frombuffer(image1.read(), np.uint8), cv2.IMREAD_COLOR)
-    img2 = cv2.imdecode(np.frombuffer(image2.read(), np.uint8), cv2.IMREAD_COLOR)
+    #img1 = cv2.imdecode(np.frombuffer(image1.read(), np.uint8), cv2.IMREAD_COLOR)
+    #img2 = cv2.imdecode(np.frombuffer(image2.read(), np.uint8), cv2.IMREAD_COLOR)
     
     # Compare faces with DeepFace
     result = DeepFace.verify(img1, img2, model_name='Facenet')
